4d006f7ed792a7c833f3fcd01289d822da129b43
AoEiuV020 <aoeiuv020@gmail.com>
2023-04-22 22:13:53 +0800

    dart solution 27

--- dart/lib/s27.dart
+++ dart/lib/s27.dart
@@ -1,13 +1,6 @@
 class Solution {
   int removeElement(List<int> nums, int val) {
-    int p = 0;
-    for (int i = 0; i < nums.length; ++i) {
-      var n = nums[i];
-      if (n == val) {
-        continue;
-      }
-      nums[p++] = n;
-    }
-    return p;
+    nums.removeWhere((element) => element == val);
+    return nums.length;
   }
 }
